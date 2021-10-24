36e2ba26f124cec5882fe9342f2bf9dddb277d8a
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-10-23 22:50:44 -0300

    Fixed an issue where the date would not get stored in local time

--- src/controllers/entries.js
+++ src/controllers/entries.js
@@ -1,8 +1,10 @@
 import connection from "../database/database.js";
-
+import dayjs from "dayjs";
+import '../../node_modules/dayjs/locale/pt-br.js';
 
 
 export async function postEntry (req, res) {
+    dayjs.locale('pt-br');
     const {
         description,
         value,
@@ -21,7 +23,7 @@ export async function postEntry (req, res) {
         }
 
         const userId = JSON.stringify(user.rows[0].userId);
-        await connection.query(`INSERT INTO entries ("userId", description, date, value) VALUES ($1, $2, $3, $4);`, [userId, description, date, value])
+        await connection.query(`INSERT INTO entries ("userId", description, date, value) VALUES ($1, $2, $3, $4);`, [userId, description, dayjs(date).format('YYYY-MM-DD'), value])
         return res.sendStatus(201);
 
     } catch (error) {
@@ -43,7 +45,7 @@ export async function getEtries (req, res) {
         }
 
         const userId = JSON.stringify(user.rows[0].userId);
-        const entries = await connection.query(`SELECT * FROM entries WHERE "userId" = $1;`, [userId]);
+        const entries = await connection.query(`SELECT * FROM entries WHERE "userId" = $1 ORDER BY date DESC;`, [userId]);
         res.send(entries.rows).status(200);
     } catch (error) {
         console.log(error);
